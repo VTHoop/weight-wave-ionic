2d4fff111d66d85d0ce2f63830f09e8a686c9f43
Patrick Hooper <patrick.hooper.m@outlook.com>
2023-01-28 10:05:57 -0500

    change brand name

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.weightmate.ionic',
-  appName: 'Weight Mate',
+  appId: 'com.weightwave.app',
+  appName: 'Weight Wave',
   webDir: 'www',
   bundledWebRuntime: false,
 };
